37d34f3e99cc7a32f216252737ee56b6e6e135f7
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2019-03-11 21:17:05 +0100

    first

---
+++ samples/first.ash
@@ -0,0 +1,33 @@
+import (
+'express' as webapp
+'twitter'
+)
+
+class Message
+    func constructor(@name, @content)
+        csl.log(`${name} - ${content}`)
+
+    func getMessage()
+        return [@name, @content]
+
+func randomString(number)
+    _alphabet: "1234567890azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN"
+    $random: ""
+
+    for $i: 0, i < number, i++
+        random: random + alphabet[Math.floor(Math.random() * Math.floor(alphabet.length))]
+
+    return random
+
+_json:: {
+    key: "valeur"
+}!:
+
+_randomNumber: Math.random()
+
+if randomNumber < 0.5
+    csl.log(randomString(15))
+elsif randomNumber < 0.7
+    csl.log(json.key)
+else
+    csl.log(":)")
---
+++ src/ashlang.js
@@ -0,0 +1,287 @@
+const fs = require("fs");
+
+const reg = {
+    // Regex name
+    // |     Regex
+    // |     |
+    // v     v
+    import: /import \(((\n|\r|'|"|[a-zA-Z]| |\d|\.)+)\)(.*)/gms,
+    subImport: /(("|')+)(([a-zA-Z]| |\d|_|"|'|`)+)(("|')+)(( |)+)(([a-zA-Z]| |)+)/gms,
+    func: /^func (([a-zA-Z]| |\d|_)+)\(((|[a-zA-Z]|@| |\d|,)+)\)/gms,
+    letvar: /^\$(([a-zA-Z]|\d|_)+):(.*)/gm,
+    thisvar: /^@(([a-zA-Z]|\d|_)+):(.*)/gm,
+    constvar: /^_(([a-zA-Z]|\d|_)+):(.*)/gm,
+    letAll: /^(([a-zA-Z]|\d|_)+)(.):(.*)/gm,
+    letPlus: /^(([a-zA-Z]|\d|_)+)\+\+/gm,
+    letMinus: /^(([a-zA-Z]|\d|_)+)--/gm,
+    letChange: /^(([a-zA-Z]|\d|_)+):(.*)/gm,
+    longLetvar: /^\$(([a-zA-Z]|\d|_)+)::(.*)/gm,
+    longLetconst: /^_(([a-zA-Z]|\d|_)+)::(.*)/gm,
+    for: /^for (([a-zA-Z]|\d|:| |\.|_|\$)+),(([a-zA-Z]|\d|:| |_|=|<|>)+),(([a-zA-Z]|\d|:| |_|\$|\+|\-|\/|\*|\.)+)/gm,
+    return: /^return (.*)/gm,
+    funcParamSet: /@(([a-zA-Z]|\d|_)+)/gms,
+    var: /var (([a-zA-Z]| |\d|_)+)=(([a-zA-Z]| |\d|_|"|'|`)+);/mgs,
+    console: /^csl\.(.*)/gm,
+    if: /^if (.*)/gm,
+    class: /^class (([a-zA-Z]|\d|_)+)/gm,
+    intfunc: /(([a-zA-Z]| |\d|_)+)\((([a-zA-Z]| |\d|_|"|'|`)+)\);/mgs
+};
+
+function test(regex, string){
+    regex.lastIndex = 0;
+    return regex.test(string);
+}
+
+function execute(regex, string){
+    regex.lastIndex = 0;
+    return regex.exec(string);
+}
+
+
+//lastC = last character at the end of each sentence if you want to have a nice code
+const lastC = "\n";
+
+/**
+ * @param {string} file 
+ * @param {string} out 
+ */
+function parse(file, out){
+    fs.readFile(file, "utf-8", function(err, buf){
+        let ashProg = buf.toString();
+        let javascriptProg = "";
+
+        let nextProg = ""; // contains the program after imports in the variable ashProg
+
+        //Import management
+        if(test(reg.import, ashProg)){
+            let inImport = execute(reg.import, ashProg);
+
+            nextProg = inImport[3];
+
+            //
+            while ((x = reg.subImport.exec(inImport[1])) !== null){
+                let req = "const ";
+
+                //To avoid infinite loops
+                if (x.index === reg.subImport.lastIndex) {
+                    reg.subImport.lastIndex++;
+                }
+
+                if(x[9] === ""){
+                    req += x[3];
+                } else {
+                    req += x[9].substr(3, x[9].length);
+                }
+
+                req += ` = require('${x[3]}');`+lastC;
+
+                javascriptProg += req;
+            }
+        } else {
+            nextProg = ashProg;
+        }
+        //Import END
+
+        //We will analyze the program line by line
+        const lines = nextProg.replace(/\r/g, "").split("\n");
+
+        //lastT (last Tabs) will count the number of tabs made each time
+        let lastT = [];
+
+        //to support variables on multiple lines
+        let longVar = false;
+
+        //to ignore the processing of the line by regex
+        let ignoreDown = false;
+
+        //to detect if a line is in a class
+        let inClass = null;
+
+        let useLastC = true;
+
+
+        lines.forEach((line, index) => {
+            //scLine = scaled down line
+            let scLine = line.trim();
+
+            //         Ignore empty line
+            //         |                    Ignore line which begin by #
+            //         |                    |
+            //         v                    v
+            if(scLine !== "" && scLine[0] !== "#"){
+                //To delimit code blocks
+                let numberOfTab = line.split("    ").length - 1;
+
+                if(lastT.length !== 0){
+                    let tmplastT = Object.assign([], lastT);
+                    tmplastT.reverse().map(x => {
+                        if(x >= numberOfTab){
+                            //else block
+                            if(lines[index].trim() === "else"){
+                                javascriptProg += tab("    ", x)+"} else {"+lastC;
+                                ignoreDown = true;
+                            } /** elseif block */ else if(lines[index].trim().substr(0, 5) === "elsif"){
+                                let tmp = execute(reg.if, scLine.substr(3));
+
+                                javascriptProg += `} else if(${tmp[1]}){`+lastC;
+                                ignoreDown = true;
+                            } /** end block */ else {
+                                if(x === inClass){
+                                    inClass = null;
+                                }
+                                javascriptProg += tab("    ", x)+"}"+lastC;
+                                lastT.pop();
+                            }
+                        }
+                    });
+                }
+
+                //add tab
+                if(!ignoreDown) javascriptProg += lastC !== null ? tab("    ", numberOfTab) : ""; else ignoreDown = false;
+
+                //Parse lines
+                //if longvar is not closed
+                if(longVar){
+                    if(scLine.substr(scLine.length - 2) === "!:"){
+                        longVar = false;
+                        javascriptProg += scLine.substr(0, scLine.length - 2)+";";
+                    } else {
+                        javascriptProg += scLine;
+                    }
+                } else if(test(reg.class, scLine)){ // class
+                    let tmp = execute(reg.class, scLine);
+
+                    //indicates that a block of code is open
+                    lastT.push(numberOfTab);
+
+                    inClass = numberOfTab;
+                    javascriptProg += `class ${tmp[1]} {`;
+                } else if(test(reg.func, scLine)){ // func -> function
+                    let tmp = execute(reg.func, scLine);
+
+                    //indicates that a block of code is open
+                    lastT.push(numberOfTab);
+
+                    if(inClass === null){
+                        javascriptProg += `function ${tmp[1]}(${tmp[3]}){`;
+                    } else {
+                        javascriptProg += `${tmp[1]}(${tmp[3].replace(/@/g, "")}){`+lastC;;
+                        let tmp_;
+
+                        while((tmp_ = reg.funcParamSet.exec(tmp[3])) !== null){
+                            javascriptProg += tab("    ", numberOfTab+1)+`this.${tmp_[1]} = ${tmp_[1]};`+lastC;
+                        }
+
+                        reg.funcParamSet.lastIndex = 0;
+                    }
+                } else if(test(reg.if, scLine)){ // if
+                    let tmp = execute(reg.if, scLine);
+
+                    lastT.push(numberOfTab);
+                    javascriptProg += `if(${tmp[1]}){`;
+                } else if(test(reg.for, scLine)){ // for
+                    let tmp = execute(reg.for, scLine);
+
+                    lastT.push(numberOfTab);
+                    javascriptProg += `for(${parseVar(tmp[1])}${tmp[3]};${tmp[5]}){`;
+                } else if(test(reg.longLetvar, scLine)){ // long let
+                    let tmp = execute(reg.longLetvar, scLine);
+
+                    longVar = true;
+                    javascriptProg += `let ${tmp[1]} =${tmp[3]}`;
+                } else if(test(reg.longLetconst, scLine)){ // long const
+                    let tmp = execute(reg.longLetconst, scLine);
+
+                    longVar = true;
+                    javascriptProg += `const ${tmp[1]} =${tmp[3]}`;
+                } else if(test(reg.letvar, scLine) || test(reg.thisvar, scLine) || test(reg.letChange, scLine) || test(reg.letAll, scLine) || test(reg.letPlus, scLine) || test(reg.letMinus, scLine) || test(reg.constvar, scLine)){ // var
+                    javascriptProg += parseVar(scLine);
+                } else if(test(reg.console, scLine)){ // console
+                    let tmp = execute(reg.console, scLine);
+
+                    javascriptProg += `console.${parseThis(tmp[1])};`;
+                } else if(test(reg.return, scLine)){ // return
+                    let tmp = execute(reg.return, scLine);
+
+                    javascriptProg += `return ${parseThis(tmp[1])};`;
+                } else {
+                    useLastC = false;
+                }
+            } else {
+                useLastC = false;
+            }
+
+            javascriptProg += useLastC ? lastC : "";
+            useLastC = true;
+        });
+
+        javascriptProg += lastT.map(x => {
+            return tab("    ", x)+"}"+lastC;
+        });
+
+        fs.writeFile(out, javascriptProg, function(err, data) {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
+    });
+}
+
+/**
+ * @param {string} c
+ * @param {number} n 
+ * @returns {string}
+ */
+function tab(c, n){
+    let p = "";
+    [...Array(n).keys()].forEach(x => {
+        p += c;
+    })
+
+    return p;
+}
+
+/**
+ * @param {string} string 
+ */
+function parseThis(string){
+    return string.replace(reg.funcParamSet, "this.$1")
+}
+
+/**
+ * @param {string} line 
+ */
+function parseVar(line){
+    if(test(reg.letvar, line)){ //$ -> let
+        let tmp = execute(reg.letvar, line);
+
+        return `let ${tmp[1]} =${tmp[3]};`;
+    } else if(test(reg.constvar, line)){ //_ -> const
+        let tmp = execute(reg.constvar, line);
+
+        return `const ${tmp[1]} =${tmp[3]};`;
+    } else if(test(reg.thisvar, line)){ //@ -> this.
+        let tmp = execute(reg.thisvar, line);
+
+        return `this.${tmp[1]} =${tmp[3]};`;
+    } else if(test(reg.letChange, line)){ //let:
+        let tmp = execute(reg.letChange, line);
+
+        return `${tmp[1]} =${tmp[3]};`;
+    } else if(test(reg.letAll, line)){ //let all
+        let tmp = execute(reg.letAll, line);
+
+        return `${tmp[1]} ${tmp[3]}=${tmp[4]};`;
+    } else if(test(reg.letPlus, line)){ //let++
+        let tmp = execute(reg.letPlus, line);
+
+        return `${tmp[1]}++;`;
+    } else if(test(reg.letMinus, line)){ //let--
+        let tmp = execute(reg.letMinus, line);
+
+        return `${tmp[1]}--;`;
+    } else {
+        return null;
+    }
+}
+
+parse("file.ash", "dist.js");
